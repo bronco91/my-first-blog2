379efe9b1ab9f896543b2c7bdb6547c926594c4c
bronco <tept06@yahoo.com>
2017-08-30 00:51:16 -0300

    Cambie el HTML para la página.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls import include, url
+from django.contrib import admin
 
 urlpatterns = [
     # Examples:
@@ -7,4 +9,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
 ]
